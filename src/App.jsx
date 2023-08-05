bd87543c33b4b75502866378999b1867a750f717
Rocio-Peralta <1rocioperalta@gmail.com>
2023-08-05 17:13:52 +1200

    complete game

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './App.css'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './components/constants'
+import { checkWinner } from './logic/board' 
+import { WinnerModal } from './components/WinnerModal'
+import { checkEndGame } from './logic/board'
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.x)
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.x)
+    setWinner(null)
+  }
+
+
+
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+      confetti()
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+  return (
+    <>
+      <main className="board">
+        <h1>Tic Tac Toe</h1>
+        <section className="game">
+          {board.map((_, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            )
+          })}
+        </section>
+        <section className="turn">
+          <Square isSelected={turn === TURNS.x}>{TURNS.x}</Square>
+          <Square isSelected={turn === TURNS.o}>{TURNS.o}</Square>
+        </section>
+        <WinnerModal resetGame={resetGame} winner={winner}/>
+      </main>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+export const Square = ({ children, updateBoard, isSelected, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
